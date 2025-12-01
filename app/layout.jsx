e4c7cba65c89480c985e722a9d5040639ea15c0f
dalaiochir <b.daagii111@gmail.com>
2025-12-01 09:09:29 +0700

    upgrade

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,16 +1,15 @@
-import '../globals.css';
+// app/layout.jsx
+import '../globals.css'; // globals.css нь app хавтас дотор байгаа тохиолдолд энэ зам зөв
 
 export const metadata = {
-  title: 'Болзооны санал',
-  description: 'Romantic bolzoonii website',
+  title: 'Date App',
+  description: 'Romantic date app',
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="mn">
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
